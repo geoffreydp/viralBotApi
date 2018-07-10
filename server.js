cc8e551b684224086bd948a009397391f12b397a
geoffreydp <geoffrey@botfabrica.com>
2018-07-10 10:08:29 -0500

    Changed to MySql data engine

--- server.js
+++ server.js
@@ -1,48 +1,39 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var mongoose = require('mongoose');
-// mongoose instance connection url connection
-//mongoose.connect('mongodb://localhost/Shares');
-
-//Mongodb atlas connection string
-/*var uri = 'mongodb://caudalia-wine-bot_usr1:WINE4All2018@' +
-          'botfabricacluster0-shard-00-00-ns9co.mongodb.net:27017,' +
-          'botfabricacluster0-shard-00-01-ns9co.mongodb.net:27017,' +
-          'botfabricacluster0-shard-00-02-ns9co.mongodb.net:27017/viralBotShare' +
-          'ssl=true&replicaSet=botfabricaCluster0-shard-0&authSource=admin&retryWrites=true';*/
-
-//mLab connection string
-var uri = 'mongodb://caudalia-wine-bot_usr1:WINE4All2018@ds259820.mlab.com:59820/viralbotshares';
-mongoose.connect(uri);
-// Create a model
-var Schema = mongoose.Schema;
-var shareLog = new Schema({
-  mcuid: {
-    type: Number,
-    required: 'Please enter users mcuid',
-    default: 1
-  },
-  count: {
-    type: Number,
-    default: 1
-  }
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "botviral"
 });
-// Create an instance of the model we created before
-var caudaliaShare = mongoose.model('caudaliaShare', shareLog);
+
 // Read the url arguements
-app.get('/api/users', function(req, res) {
-  var user_id = req.query.mcuid;
-// Create a database entry from the read params and save it to mongodb using mongoose
-  var new_share = new caudaliaShare({ mcuid: req.query.mcuid});
-  new_share.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
+app.get('/share', function(req, res) {
+  con.connect(function(err) {
+    if (err) throw err;
+// Register the share has led to a CTA
+    var sql = "INSERT INTO shares (campaignid, user, shares) VALUES (" + req.query.cpn + " ," + req.query.usr + ", 1)";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log ("1 record inserted");
+    });
+// Get the url from the campaign
+    var sql = "select url from campaign where id = " + req.query.cpn;
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+      Object.keys(result).forEach(function(key) {
+        var row = result[key];
+        console.log(row.url);
+// Redirect to the camapaign url
+        res.redirect(row.url);
+      });
+    });
+  });
 });
-// Send a 302 redirect towards the bot flow using refurl
-  res.redirect('https://m.me/CaudaliaWineBox?ref=Mundial_au');
-});
+
 
 app.listen(port);
 
-console.log('viralBotApi2 RESTful API server started on: ' + port);
+console.log('botViral RESTful API server started on: ' + port);
