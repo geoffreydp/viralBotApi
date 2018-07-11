dd8f43c1ec04aec5fd5170c397f01e5a46bf2cef
geoffreydp <geoffrey@botfabrica.com>
2018-07-10 22:35:24 -0500

    added /view route and removed connection from the functions to solve handshake error on repeated calls

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ var con = mysql.createConnection({
   password: "",
   database: "botviral"
 });
+// Create the database connection once, reuse later
+con.connect(function(err) {
+  if (err) throw err;
+});
 
 // Read the url arguements
 app.get('/share', function(req, res) {
-  con.connect(function(err) {
-    if (err) throw err;
 // Register the share has led to a CTA
     var sql = "INSERT INTO shares (campaignid, user, shares) VALUES (" + req.query.cpn + " ," + req.query.usr + ", 1)";
     con.query(sql, function (err, result) {
@@ -30,9 +32,16 @@ app.get('/share', function(req, res) {
         res.redirect(row.url);
       });
     });
-  });
 });
 
+app.get('/view', function (req, res) {
+  // Get all the shares registerd for a specific customer
+  var sql ="SELECT * FROM sharecount WHERE custid = " + req.query.cid
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 app.listen(port);
 
